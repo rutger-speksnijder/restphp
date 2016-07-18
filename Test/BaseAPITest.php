ca8c56096b2e893cb19739ec2199834e98a1ef17
Rutger Speksnijder <rutgerspeksnijder@hotmail.com>
2016-07-18 16:22:20 +0200

    Updated gitignore and tests

--- Test/BaseAPITest.php
+++ Test/BaseAPITest.php
@@ -5,8 +5,5 @@ use \RestPHP\Router;
 use \RestPHP\BaseAPI;
 
 class BaseAPITest extends \PHPUnit_Framework_TestCase {
-	public function testCanNotCreateTokenServerWithInvalidArguments() {
-		$this->setExpectedException('Exception');
-		$server = BaseAPI::createTokenServer(false, null, null, null);
-	}
+
 }
